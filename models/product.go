a246f863e8074cc401a27511ca6c3e080db34e7d
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-04-10 22:32:34 +0700

    feat:add beforeUpdate for product validate

--- models/product.go
+++ models/product.go
@@ -25,3 +25,15 @@ func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
 	err = nil
 	return
 }
+
+func (p *Product) BeforeUpdate(tx *gorm.DB) (err error) {
+	_, errCreate := govalidator.ValidateStruct(p)
+
+	if errCreate != nil {
+		err = errCreate
+		return
+	}
+
+	err = nil
+	return
+}
